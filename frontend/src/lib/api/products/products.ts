c4e986a910030857d4158583dd10b968eec851f8
jithingeorgedev <jithin.george.el@gmail.com>
2025-05-04 20:45:19 +0530

    fake api env hardcoded

--- frontend/src/lib/api/products/products.ts
+++ frontend/src/lib/api/products/products.ts
@@ -3,7 +3,7 @@
 
 export const fetchProductsAPI = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_FAKE_API_PRODUCTS}/api/products`);
+    const response = await fetch(`https://fakestoreapi.in/api/products`);
     return response;
   } catch (error: any) {
     throw error.response?.data || { message: 'Login failed' };
